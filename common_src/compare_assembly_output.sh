2154b14871f72ed1dbae1b29af525a25dd15ab9c
ERIK ALAPAEAE <erik.alapaa@gmail.com>
2026-01-11 13:36:08 +0100

    Minor script polishing

--- common_src/compare_assembly_output.sh
+++ common_src/compare_assembly_output.sh
@@ -5,6 +5,7 @@ git log | head -n 6
 CC=/home/alapaa/renesas/ra/e2studio_v2025-10_fsp_v6.2.0/toolchains/gcc_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc
 echo "Using compiler: "${CC}
 
+PROJDIR=test1_cpp
 OUTPUTDIR=/tmp
 FILE_NAME_BASE=perf_meas_loop.assemblyoutput
 O_0_FILE=${OUTPUTDIR}/${FILE_NAME_BASE}-O0.S
@@ -12,9 +13,9 @@ O_s_FILE=${OUTPUTDIR}/${FILE_NAME_BASE}-Os.S
 O_2_FILE=${OUTPUTDIR}/${FILE_NAME_BASE}-O2.S
 
 echo "Creating assembler output..."
-$CC -I ./ra_gen/  -O0 -fverbose-asm -S src/perf_meas_loop.c -o ${O_0_FILE}
-$CC -I ./ra_gen/  -Os -fverbose-asm -S src/perf_meas_loop.c -o ${O_s_FILE}
-$CC -I ./ra_gen/  -O2 -fverbose-asm -S src/perf_meas_loop.c -o ${O_2_FILE}
+$CC -I ../${PROJDIR}/ra_gen/  -O0 -fverbose-asm -S perf_meas_loop.cpp -o ${O_0_FILE}
+$CC -I ../${PROJDIR}/ra_gen/  -Os -fverbose-asm -S perf_meas_loop.cpp -o ${O_s_FILE}
+$CC -I ../${PROJDIR}/ra_gen/  -O2 -fverbose-asm -S perf_meas_loop.cpp -o ${O_2_FILE}
 echo "Done!"
 
 echo "Function calls in O0:"

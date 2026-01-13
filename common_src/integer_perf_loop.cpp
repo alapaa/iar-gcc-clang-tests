740b07c3a9a5ca60c307326d5ddc20c007be2ad6
ERIK ALAPAEAE <erik.alapaa@gmail.com>
2026-01-13 17:04:51 +0100

    Minor polishing

---
+++ common_src/integer_perf_loop.cpp
@@ -0,0 +1,31 @@
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+
+extern "C" {
+#include "integer_perf_loop.h"
+}
+#include "systick_util.h"
+
+extern "C" {
+uint64_t u64_external_result = 0; // To prevent gcc from optimizing away
+                                  // unused result.
+
+// Run computation for a few seconds in a tight loop.
+// Report diff of Cortex-M systicks before and after loop.
+uint32_t integer_perf_loop(uint32_t arr1[], uint32_t arr2[])
+{
+    uint64_t result = 0;
+    systick_setup();
+
+    uint32_t start_timer = systick_microseconds();
+    const int kIterations = 100000; // To be adjusted.
+    for (int i = 0; i < kIterations; i++) {
+        result += arr1[i]*arr2[i];
+    }
+    u64_external_result = result;
+    uint32_t end_timer = systick_microseconds();
+
+    return (uint32_t)(end_timer - start_timer);
+}
+}

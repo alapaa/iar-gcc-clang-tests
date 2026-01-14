80328933ca2788aef4159ba5c50bb19745e5a60b
ERIK ALAPAEAE <erik.alapaa@gmail.com>
2026-01-14 14:48:32 +0100

    Performance test 4, updated with IAR __no_init

--- common_src/integer_perf_loop.h
+++ common_src/integer_perf_loop.h
@@ -2,6 +2,11 @@
 extern "C" {
 #endif
 #define kArrSz 10000
+#ifdef __IAR_SYSTEMS_ICC__
+#define IAR_NO_INIT __no_init
+#else
+#define IAR_NO_INIT
+#endif
 extern uint64_t u64_external_result;
 uint32_t integer_perf_loop_add_vectors(
     const uint32_t arr1[], const uint32_t arr2[], uint32_t result[]);

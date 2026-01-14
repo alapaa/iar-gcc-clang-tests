d04a82db08b3c125722137432f1fdbce8d725113
ERIK ALAPAEAE <erik.alapaa@gmail.com>
2026-01-14 10:02:36 +0100

    Added manual loop unrolling

---
+++ common_src/integer_perf_loop_add_vectors_unrolled.cpp
@@ -0,0 +1,30 @@
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+
+extern "C" {
+#include "integer_perf_loop.h"
+}
+#include "systick_util.h"
+
+extern "C" {
+// Run computation for a few seconds in a tight loop.
+// Report diff of Cortex-M systicks before and after loop.
+uint32_t integer_perf_loop_add_vectors_unrolled(
+    const uint32_t arr1[], const uint32_t arr2[], uint32_t result[])
+{
+    systick_setup();
+
+    uint32_t start_timer = systick_microseconds();
+    const int kIterations = 100000; // To be adjusted.
+    for (int i = 0; i < kIterations; i += 4) {
+        result[i] = arr1[i]+arr2[i];
+        result[i+1] = arr1[i+1]+arr2[i+1];
+        result[i+2] = arr1[i+2]+arr2[i+2];
+        result[i+3] = arr1[i+3]+arr2[i+3];
+    }
+    uint32_t end_timer = systick_microseconds();
+
+    return (uint32_t)(end_timer - start_timer);
+}
+}
